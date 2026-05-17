40d333d2bbc37a245f079c8b3a4a50efb3bb3619
santosh7017 <-v>
2026-05-17 16:22:42 +0530

    HomeBrew setup

---
+++ androidfilesync.rb
@@ -0,0 +1,17 @@
+cask "androidfilesync" do
+  version "2.0"
+  sha256 "7b3ec47f39c3d64d5faab9f654e9c450a30a1acd068195f0765b40640673dba8"
+
+  url "https://github.com/Santosh7017/AndroidFileSync/releases/download/v#{version}/AndroidFileSync.dmg"
+  name "AndroidFileSync"
+  desc "Free, native macOS app to transfer files and manage Android devices over WiFi or USB"
+  homepage "https://santosh7017.github.io/AndroidFileSync/"
+
+  app "AndroidFileSync.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.santosh.AndroidFileSync.plist",
+    "~/Library/Saved Application State/com.santosh.AndroidFileSync.savedState",
+  ]
+end
+
