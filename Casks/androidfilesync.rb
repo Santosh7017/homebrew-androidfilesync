4d8f124387f6e067dfc8391611b1cef622513f85
Santosh  Morya <73546536+Santosh7017@users.noreply.github.com>
2026-05-19 00:16:42 +0530

    version update

--- Casks/androidfilesync.rb
+++ Casks/androidfilesync.rb
@@ -1,7 +1,7 @@
 cask "androidfilesync" do
-  version "2.1.0"
-  sha256 "010158f901372ee13f58bf04d8b0d5374dffed7276cef35b3e2c1807246a802f" 
-  url "https://github.com/Santosh7017/AndroidFileSync/releases/download/V#{version}/AndroidFileSync.dmg"
+  version "2.2.0"
+  sha256 "eea3c81990f336166704b49163067f8647ce641643b5150bcafc3eab7a820400" 
+  url "https://github.com/Santosh7017/AndroidFileSync/releases/download/v#{version}/AndroidFileSync.dmg"
   name "AndroidFileSync"
   desc "Free, native macOS app to transfer files and manage Android devices over WiFi or USB"
   homepage "https://santosh7017.github.io/AndroidFileSync/"
