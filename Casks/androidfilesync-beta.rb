7a05ed017b7cc0d3be57022db10332624b1c99fa
Santosh  Morya <73546536+Santosh7017@users.noreply.github.com>
2026-08-05 01:18:55 +0530

    Update sha256 checksum for androidfilesync-beta

--- Casks/androidfilesync-beta.rb
+++ Casks/androidfilesync-beta.rb
@@ -1,6 +1,6 @@
 cask "androidfilesync-beta" do
   version "2.3.2-beta.1"
-  sha256 "f247a516096b7c6319f5707494b726a8fc6640c53adcba3823e9f0c0ab5487b2" 
+  sha256 "11ebc216ac6705fe1131ad4e05223dcf60bc5d70b293a079ec07f33d1e446f42" 
   url "https://github.com/Santosh7017/AndroidFileSync/releases/download/v#{version}/AndroidFileSync.dmg"
   name "AndroidFileSync Beta"
   desc "Free, native macOS app to transfer files and manage Android devices over WiFi or USB (Beta)"
